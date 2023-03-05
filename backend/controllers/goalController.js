1567911199e78a4303cb3a7b781d4833d824513b
Kara <kara_himanshu@yahoo.com>
2023-03-05 17:22:09 +0530

    Goals controller and routes setup

--- backend/controllers/goalController.js
+++ backend/controllers/goalController.js
@@ -1,26 +1,32 @@
+const asyncHandler = require("express-async-handler");
+
 // @desc Get goals
 // @route GET api/goals
 // @access private
-const getGoals = (req, res) => {
+const getGoals = asyncHandler(async (req, res) => {
   res.status(200).json({ message: "Get Goals" });
-};
+});
 // @desc Set goals
 // @route POST api/goals
 // @access private
-const setGoal = (req, res) => {
+const setGoal = asyncHandler(async (req, res) => {
+  if (!req.body.text) {
+    res.status(400);
+    throw new Error("Please add a text");
+  }
   res.status(200).json({ message: "Set Goals" });
-};
+});
 // @desc update goals
 // @route PUT api/goals/:id
 // @access private
-const updateGoal = (req, res) => {
+const updateGoal = asyncHandler(async (req, res) => {
   res.status(200).json({ message: `update Goal ${req.params.id}` });
-};
+});
 // @desc DELETE goals
 // @route DELETE/:ID api/goals
 // @access private
-const deleteGoal = (req, res) => {
+const deleteGoal = asyncHandler(async (req, res) => {
   res.status(200).json({ message: `delete Goal ${req.params.id}` });
-};
+});
 
 module.exports = { getGoals, setGoal, updateGoal, deleteGoal };
--- backend/server.js
+++ backend/server.js
@@ -1,10 +1,16 @@
 const express = require("express");
 const dotenv = require("dotenv").config();
+
+const { errorHandler } = require("./middleware/errorMiddleware");
 const port = 8000;
 
 const app = express();
 
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+
 app.use("/api/goals", require("./routes/goalRoutes"));
+app.use(errorHandler);
 
 app.listen(port, () => {
   console.log(`Welcome to port ${port}`);
